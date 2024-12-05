dc9ba22808bac865dc42886c89f0d248b7ff1f5f
Sergey Konnov <sergeyzorro007@gmail.com>
2024-12-05 22:23:38 +0300

    update MLP and NN models

---
+++ iOpt/models/model_mlp.py
@@ -0,0 +1,58 @@
+from iOpt.models.model import Model
+from iOpt.method.search_data import SearchDataItem
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.neural_network import MLPClassifier
+from imblearn.over_sampling import RandomOverSampler
+import torch.optim as optim
+import random as rd
+class ModelMLPProba(Model):  # scaled, adjusted weights
+    iter = 0
+    def __init__(self):
+        super().__init__()
+        self.is_fit = False
+        # self.model = xgboost.XGBClassifier(n_estimators=5, max_depth=2, objective='binary:logistic', device="cuda")
+        num_elems = 200
+        alpha = 0.1
+        self.model = MLPClassifier(alpha=alpha, hidden_layer_sizes=(num_elems,num_elems),
+                              solver='lbfgs', activation='logistic', max_iter=1000, random_state=42)
+
+        self.scaler = MinMaxScaler()
+
+
+    def fit(self, X: list, y: list):
+        scaled_X = self.scaler.fit_transform(X)
+
+        # experimental!
+        # oversample = RandomOverSampler(sampling_strategy='minority')
+        # scaled_X, y = oversample.fit_resample(scaled_X, y)
+        # print("Oversampled class distribution:", len(y))
+        ModelMLPProba.iter += 1
+        print("MLP fit #", ModelMLPProba.iter)
+
+        # scaled_X, y = zip(*rd.shuffle(zip(scaled_X, y)))
+        self.model.fit(scaled_X, y)
+
+        self.is_fit = True
+
+    def calculate_dot_characteristic(self, *point) -> float:
+        p = self.model.predict_proba([point])
+        return p[0][1]
+
+    def calculate_r_ps(self, curr_point: SearchDataItem, left_point: SearchDataItem):
+        if not self.is_fit:
+            return 0.
+
+        p1 = self.calculate_dot_characteristic(*[pt.value for pt in left_point.function_values])
+        p2 = self.calculate_dot_characteristic(*[pt.value for pt in curr_point.function_values])
+        # p1 -= 0.5
+        # p2 -= 0.5
+
+        r_ps = (p1 + p2) / 2 # [1]?
+        return r_ps
+
+    def get_model(self):
+        return self.model
+
+    def name(self):
+        return "MLP"
--- iOpt/models/model_nn.py
+++ iOpt/models/model_nn.py
@@ -7,24 +7,28 @@ from torch import nn, Tensor
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
 class Net(nn.Module):
     def __init__(self, N: int):
         super().__init__()
-        self.lin1 = nn.Linear(N, 512)
-        self.lin2 = nn.Linear(512, 512)
-        self.lin3 = nn.Linear(512, 2)
-        self.softmax = nn.Softmax()
+        self.lin1 = nn.Linear(N, 128)
+        self.lin2 = nn.Linear(128, 64)
+        self.lin3 = nn.Linear(64, 2)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = nn.functional.relu(self.lin1(x))
-        x = nn.functional.relu(self.lin2(x))
-        x = nn.functional.relu(self.lin3(x))
+        x = torch.relu(self.lin1(x))
+        x = torch.relu(self.lin2(x))
+        x = self.lin3(x)
         x = self.softmax(x)
         return x
 
 
 class ModelNNProba(Model):  # scaled, adjusted weights
+    iter = 0
+    cnt0 = 0
+    cnt1 = 0
     def __init__(self):
         super().__init__()
         self.is_fit = False
@@ -41,21 +45,28 @@ class ModelNNProba(Model):  # scaled, adjusted weights
         # self.flatten = nn.Flatten()
 
         self.net = Net(2)
-        self.criterion = nn.CrossEntropyLoss(torch.Tensor([0.8, 0.2]).to(torch.float32))  # change weigths?
-        self.optimizer = optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
+        self.criterion = nn.CrossEntropyLoss()  # change weigths?
+        # self.criterion = nn.CrossEntropyLoss(torch.Tensor([0.2, 0.8]).to(torch.float32))  # change weigths?
+        # self.optimizer = optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
+        self.optimizer = optim.Adam(self.net.parameters(), lr=0.001)
 
         self.scaler = MinMaxScaler()
 
     def fit(self, X: list, y: list):
+        print("0s:", ModelNNProba.cnt0, ", 1s: ", ModelNNProba.cnt1)
         scaled_X = self.scaler.fit_transform(X)
 
-        self.optimizer.zero_grad()
+        scaled_X = torch.FloatTensor(scaled_X)
+        y = torch.LongTensor(y)
+        dataset = TensorDataset(scaled_X, y)
+        data_loader = DataLoader(dataset, batch_size=2, shuffle=True)
 
-        for epoch in range(2):  # loop over the dataset multiple times
+        for epoch in range(20):  # loop over the dataset multiple times
             running_loss = 0.0
-            for i in range(len(scaled_X)):
-                outputs = self.net(torch.from_numpy(scaled_X[i]).to(torch.float32))
-                loss = self.criterion(outputs, torch.Tensor([y[i], 1-y[i]]).to(torch.float32))
+            for batch_X, batch_y in data_loader:
+                self.optimizer.zero_grad()  # reset gradients
+                outputs = self.net(batch_X)
+                loss = self.criterion(outputs, batch_y)
                 loss.backward()
                 self.optimizer.step()
 
@@ -64,13 +75,22 @@ class ModelNNProba(Model):  # scaled, adjusted weights
                 # if i % 1 == 0:    # print every 2000 mini-batches
                 #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 #     running_loss = 0.0
-
+        ModelNNProba.iter += 1
+        print("NN fit #", ModelNNProba.iter)
         self.is_fit = True
+        ModelNNProba.cnt0 = 0
+        ModelNNProba.cnt1 = 0
 
-    def calculate_dot_characteristic(self, *point):
-        p = self.net(torch.Tensor(point).to(torch.float32))
-        # print("-->", p[0], p[1], "<--")
-        return p[0]
+    def calculate_dot_characteristic(self, *point) -> float:
+        self.net.eval()
+        p = self.net(torch.FloatTensor(self.scaler.transform([point])))
+        # print(torch.FloatTensor(self.scaler.transform([point])), "<_-_-_-_-_-", p, point, self.scaler.transform([point]))
+        if float(p[0, 1].item()) >= 0.5:
+            ModelNNProba.cnt1 += 1
+        else:
+            ModelNNProba.cnt0 += 1
+        # print("-->", p[0, 1].item(), "<--")
+        return float(p[0, 1].item())
 
     def calculate_r_ps(self, curr_point: SearchDataItem, left_point: SearchDataItem):
         if not self.is_fit:
